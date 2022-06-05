200d4eef687e880b29af06c493ef761f2dbb8e18
anindyaca <anindyachakraborty@outlook.com>
2022-06-05 19:28:02 +0530

    commit 5

--- Linear Regression.py
+++ Linear Regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as mpl
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("Salary_Data.csv")
 # print(df.head())
@@ -9,5 +10,22 @@ df = pd.read_csv("Salary_Data.csv")
 # print(df.describe()) --> gives basic stat for the dataset
 # now this is a linear regression problem, simple line graph equation is
 # y = mx + c --> here m is slope of graph(line) and c is the constant, y is output (this o/p is to be predicted using the linear regression)
+# so independent variable is X and dependent variable is Y (since this is dependent on mx + c)
+
+# now defining X & Y
+X = df['YearsExperience']
+#print(X.head())
+Y = df['Salary']
+#print(Y.head())
+
+X_train , X_test, Y_train , Y_test = train_test_split(X,Y, train_size=0.7 , random_state= 100)
 
 
+X_train = X_train[:,np.newaxis]
+X_test = X_test[:,np.newaxis]
+
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(X_train , Y_train)
+
+Y_pred = lr.predict(X_test)
