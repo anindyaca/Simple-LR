3f8ed346793047dc3d9f64f30bbc7c9446ad5b6f
anindyaca <anindyachakraborty@outlook.com>
2022-06-05 18:55:49 +0530

    commit 5

--- Linear Regression.py
+++ Linear Regression.py
@@ -7,5 +7,7 @@ df = pd.read_csv("Salary_Data.csv")
 # print(df.head())
 
 # print(df.describe()) --> gives basic stat for the dataset
+# now this is a linear regression problem, simple line graph equation is
+# y = mx + c --> here m is slope of graph(line) and c is the constant, y is output (this o/p is to be predicted using the linear regression)
 
 
